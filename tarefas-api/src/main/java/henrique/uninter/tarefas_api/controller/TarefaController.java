1a22d38911c73048d5869e09b8d5bd43713690ee
Henrique Rocha <henriquesoaresrocha@gmail.com>
2025-09-08 21:36:17 -0300

    Update TarefaController.java

--- tarefas-api/src/main/java/henrique/uninter/tarefas_api/controller/TarefaController.java
+++ tarefas-api/src/main/java/henrique/uninter/tarefas_api/controller/TarefaController.java
@@ -2,11 +2,9 @@ package henrique.uninter.tarefas_api.controller;
 
 
 import henrique.uninter.tarefas_api.repository.TarefaRepository;
-
 import henrique.uninter.tarefas_api.model.Tarefa;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -14,18 +12,15 @@ import java.util.List;
 public class TarefaController {
 
     private final TarefaRepository repository;
-
-
+    
     public TarefaController(TarefaRepository repository) {
         this.repository = repository;
     }
-
 
     @GetMapping
     public List<Tarefa> listar() {
         return repository.findAll();
     }
-
 
     @GetMapping("/{id}")
     public ResponseEntity<Tarefa> buscarPorId(@PathVariable Long id) {
@@ -34,13 +29,11 @@ public class TarefaController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
-
     @PostMapping
     public ResponseEntity<Tarefa> criar(@RequestBody Tarefa tarefa) {
         Tarefa salva = repository.save(tarefa);
-        return ResponseEntity.status(201).body(salva); // 201 Created
+        return ResponseEntity.status(201).body(salva);
     }
-
 
     @PutMapping("/{id}")
     public ResponseEntity<Tarefa> atualizar(@PathVariable Long id, @RequestBody Tarefa dados) {
@@ -54,16 +47,16 @@ public class TarefaController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
-
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> remover(@PathVariable Long id) {
         if (repository.existsById(id)) {
             repository.deleteById(id);
-            return ResponseEntity.noContent().build(); // 204 No Content
+            return ResponseEntity.noContent().build(); 
         } else {
-            return ResponseEntity.notFound().build(); // 404 Not Found
+            return ResponseEntity.notFound().build();
         }
     }
 }
+
 
 
